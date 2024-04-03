c5bcdb1b3e1c06769fcf3c1163bc90f6c0e084c1
hugoa <hugoa@HUGO-AQUINO>
2024-04-03 11:20:15 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoWeb1.0/DataBase/ClienteContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWeb1._0.DataBase
+{
+    public class ClienteContext:DbContext
+    {
+        public ClienteContext():base("name=Cliente")
+        {
+            
+        }
+    }
+}
---
+++ ProyectoWeb1.0/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ProyectoWeb1._0.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProyectoWeb1._0.DataBase.ClienteContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProyectoWeb1._0.DataBase.ClienteContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
