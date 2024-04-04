31f39c442ded115c43926674214da450f4883846
hugoa <hugoa@HUGO-AQUINO>
2024-04-04 16:27:17 -0600

    Tablaclienteup

--- ProyectoWeb1.0/DataBase/ClienteContext.cs
+++ ProyectoWeb1.0/DataBase/ClienteContext.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ProyectoWeb1._0.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -12,5 +14,14 @@ namespace ProyectoWeb1._0.DataBase
         {
             
         }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
     }
 }
---
+++ ProyectoWeb1.0/Models/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWeb1._0.Models
+{
+    [Table("Cliente")]
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        //subida de archivo
+        //mas
+
+        [MaxLength(15)]
+        public string Codigo { get; set; }
+
+        [MaxLength(50)]
+        public string Nombres { get; set; }
+
+        [MaxLength(50)]
+        public string Apellidos { get; set; }
+
+        public int GrupoDescuentoId { get; set; }
+
+        public int CondicionPagoId { get; set; }
+
+        public bool Estado { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
