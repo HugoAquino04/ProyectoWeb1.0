2eabf950a5ef246f50794f2e4a34feb396190084
Maria Belen Sagastume <mariabelensasagas@outlook.com>
2024-04-04 22:29:04 -0600

    Hice un comentario en los modelos en la clase UnidadMedida

---
+++ ProyectoWeb1.0/Models/UnidadMedida.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWeb1._0.Models
+{
+    public class UnidadMedida
+    {
+        //Comentario
+    }
+}
